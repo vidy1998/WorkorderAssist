ed1bdc5a3164a6cdc086984477ed01e590ec982e
Maria <mariazakirlabeeba@gmail.com>
2025-06-12 21:58:15 -0400

    IOS App Project Created

---
+++ Frontend/WorkOrderAssistApp/WorkOrderAssistApp/WorkOrderAssistAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkOrderAssistAppApp.swift
+//  WorkOrderAssistApp
+//
+//  Created by Maria Labeeba on 2025-06-12.
+//
+
+import SwiftUI
+
+@main
+struct WorkOrderAssistAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Frontend/WorkOrderAssistApp/WorkOrderAssistAppTests/WorkOrderAssistAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  WorkOrderAssistAppTests.swift
+//  WorkOrderAssistAppTests
+//
+//  Created by Maria Labeeba on 2025-06-12.
+//
+
+import Testing
+@testable import WorkOrderAssistApp
+
+struct WorkOrderAssistAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
